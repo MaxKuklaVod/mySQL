8379dcae287d8aec73f69c3d322a66ea4eccde22
Maxim <copperious@mail.ru>
2025-02-11 20:48:19 +0800

    Finished homework

--- FirstHomework.sql
+++ FirstHomework.sql
@@ -1,41 +1,104 @@
 -- 1) Поиск данных по именам
 -- а) Robert
-SELECT * FROM `clients` WHERE name = "Robert";
+SELECT * 
+FROM `clients` 
+WHERE name = 'Robert';
+
 -- б) Jeffrey
-SELECT * FROM `clients` WHERE name = "Jeffrey";
+SELECT * 
+FROM `clients` 
+WHERE name = 'Jeffrey';
+
 -- в) John
-SELECT * FROM `clients` WHERE name = "John";
+SELECT * 
+FROM `clients` 
+WHERE name = 'John';
+
 -- г) Richard
-SELECT * FROM `clients` WHERE name = "Richard";
+SELECT * 
+FROM `clients` 
+WHERE name = 'Richard';
+
 -- д) David
-SELECT * FROM `clients` WHERE name = "David";
--- е) по всем этим значениям в одном запросе
-SELECT * FROM `clients` WHERE name in ("Robert", "Jeffrey", "John", "Richard", "David");
+SELECT * 
+FROM `clients` 
+WHERE name = 'David';
+
+-- е) Поиск по всем указанным именам в одном запросе
+SELECT * 
+FROM `clients` 
+WHERE name IN ('Robert', 'Jeffrey', 'John', 'Richard', 'David');
 
 -- 2) Поиск данных по гендерам
--- а) по мужчинам
-SELECT * FROM `clients` WHERE gender = "M";
--- б) по женщинам
-SELECT * FROM `clients` WHERE gender = "F";
+-- а) По мужчинам (gender = 'M')
+SELECT * 
+FROM `clients` 
+WHERE gender = 'M';
+
+-- б) По женщинам (gender = 'F')
+SELECT * 
+FROM `clients` 
+WHERE gender = 'F';
 
 -- 3) Поиск имени и телефона по фамилии
 -- а) Williams
-SELECT name, phone FROM `clients` WHERE lastname = "Williams";
+SELECT name, phone 
+FROM `clients` 
+WHERE lastname = 'Williams';
+
 -- б) Jones
-SELECT name, phone FROM `clients` WHERE lastname = "Jones";
+SELECT name, phone 
+FROM `clients` 
+WHERE lastname = 'Jones';
+
 -- в) Brown
-SELECT name, phone FROM `clients` WHERE lastname = "Brown";
+SELECT name, phone 
+FROM `clients` 
+WHERE lastname = 'Brown';
+
 -- г) Miller
-SELECT name, phone FROM `clients` WHERE lastname = "Miller";
--- д) по всем этим значениям в одном запросе
-SELECT name, phone FROM `clients` WHERE lastname in ("Williams", "Jones", "Brown", "Miller");
+SELECT name, phone 
+FROM `clients` 
+WHERE lastname = 'Miller';
+
+-- д) Поиск по всем указанным фамилиям в одном запросе
+SELECT name, phone 
+FROM `clients` 
+WHERE lastname IN ('Williams', 'Jones', 'Brown', 'Miller');
 
 -- 4) Поиск даты рождения и пола пользователя по имени или фамилии
 -- а) Kelly
-SELECT dbirth, gender FROM `clients` WHERE name = "Kelly" or lastname = "Kelly";
+SELECT dbirth, gender 
+FROM `clients` 
+WHERE name = 'Kelly' OR lastname = 'Kelly';
+
 -- б) Thomas
-SELECT dbirth, gender FROM `clients` WHERE name = "Thomas" or lastname = "Thomas";
+SELECT dbirth, gender 
+FROM `clients` 
+WHERE name = 'Thomas' OR lastname = 'Thomas';
+
 -- в) Clark
-SELECT dbirth, gender FROM `clients` WHERE name = "Clark" or lastname = "Clark";
+SELECT dbirth, gender 
+FROM `clients` 
+WHERE name = 'Clark' OR lastname = 'Clark';
+
 -- г) Joseph
-SELECT dbirth, gender FROM `clients` WHERE name = "Joseph" or lastname = "Joseph";
+SELECT dbirth, gender 
+FROM `clients` 
+WHERE name = 'Joseph' OR lastname = 'Joseph';
+
+-- 5) Определение полей пола, даты рождения и номера телефона по имени и фамилии
+-- а) Tom Brown
+SELECT gender, dbirth, phone 
+FROM `clients` 
+WHERE name = 'Tom' AND lastname = 'Brown';
+
+-- б) Janet Silberstein
+SELECT gender, dbirth, phone 
+FROM `clients` 
+WHERE name = 'Janet' AND lastname = 'Silberstein';
+
+-- в) John Foster
+SELECT gender, dbirth, phone 
+FROM `clients` 
+WHERE name = 'John' AND lastname = 'Foster';
