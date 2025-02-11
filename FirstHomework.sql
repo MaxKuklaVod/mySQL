44db03d5460174994ffcb783d690a84f6572ebc4
Maxim <copperious@mail.ru>
2025-02-11 18:21:41 +0800

    SecondTask

--- FirstHomework.sql
+++ FirstHomework.sql
@@ -11,3 +11,9 @@ SELECT * FROM `clients` WHERE name = "Richard";
 SELECT * FROM `clients` WHERE name = "David";
 -- е) по всем этим значениям в одном запросе
 SELECT * FROM `clients` WHERE name in ("Robert", "Jeffrey", "John", "Richard", "David");
+
+-- 2) Поиск данных по гендерам
+-- а) по мужчинам
+SELECT * FROM `clients` WHERE gender = "M";
+-- б) по женщинам
+SELECT * FROM `clients` WHERE gender = "F";
