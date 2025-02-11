647f362bd27339fad715beae61d1d46c32c65483
Maxim <copperious@mail.ru>
2025-02-11 19:02:28 +0800

    Third Task

--- FirstHomework.sql
+++ FirstHomework.sql
@@ -17,3 +17,15 @@ SELECT * FROM `clients` WHERE name in ("Robert", "Jeffrey", "John", "Richard", "
 SELECT * FROM `clients` WHERE gender = "M";
 -- б) по женщинам
 SELECT * FROM `clients` WHERE gender = "F";
+
+-- 3) Поиск имени и телефона по фамилии
+-- а) Williams
+SELECT name, phone FROM `clients` WHERE lastname = "Williams";
+-- б) Jones
+SELECT name, phone FROM `clients` WHERE lastname = "Jones";
+-- в) Brown
+SELECT name, phone FROM `clients` WHERE lastname = "Brown";
+-- г) Miller
+SELECT name, phone FROM `clients` WHERE lastname = "Miller";
+-- д) по всем этим значениям в одном запросе
+SELECT name, phone FROM `clients` WHERE lastname in ("Williams", "Jones", "Brown", "Miller");
